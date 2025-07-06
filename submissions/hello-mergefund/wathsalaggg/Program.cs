d0078189f06867366a2226e35e2017e6edb448ea
Wathsala Gamarachchi <wathsalagamarachchi@gmail.com>
2025-07-06 20:41:02 +0530

    initial C# console project with solution file

---
+++ submissions/hello-mergefund/wathsalaggg/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("🐰 Welcome to Bunny Mailbox: The MergeFund Quest!");
+        Console.WriteLine("Let’s begin our adventure...");
+    }
+}
